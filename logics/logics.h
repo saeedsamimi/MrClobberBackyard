f2048d99479a89f438a58e5876d48fc40f1aab0c
saeedsamimi <doob.fooy@gmail.com>
2023-12-26 16:18:25 +0330

    the logics not pushed

--- logics/logics.h
+++ logics/logics.h
@@ -1,14 +1,14 @@
 #pragma once
-#include "./constants.h"
-#include "./flagManager.h"
+#include "../constants.h"
+#include "../flagManager.h"
 #include "stdio.h"
-#include "./map.h"
+#include "../map.h"
 
 int canMove(int x, int y, enum MOVEMENT move) {
   int currentPos = map[y][x];
   int nextPos;
   enum CHARACTER_TYPE current_character;
-  enum CHARACTER_TYPE nextPos_character = NONE;
+  enum CHARACTER_TYPE nextPos_character = NO_CHARACTER;
   // Define next Position and Check for walls and Borders
   switch (move)
   {
@@ -52,9 +52,6 @@ int canMove(int x, int y, enum MOVEMENT move) {
     if (y + 1 >= BOARD_SIZE || x - 1 < 0) return 0;
     nextPos = map[y + 1][x - 1];
     break;
-  default:
-    return 0;
-    break;
   }
   //----------- Set Current Character -----------
   if (hasFlag(currentPos, FLAG_DOG)) {
@@ -67,6 +64,7 @@ int canMove(int x, int y, enum MOVEMENT move) {
     current_character = MOUSE_CHARACTER;
   }
   //---------- Set Next Position Character --------
+  printf("%d is the data of next pos", nextPos);
   if (hasFlag(nextPos, FLAG_DOG)) {
     nextPos_character = DOG_CHARACTER;
   }
@@ -78,9 +76,67 @@ int canMove(int x, int y, enum MOVEMENT move) {
   }
   // Condition - Check Overlap for non-cat characters 
   // Cat's can be overlaped!
-  if (nextPos_character == NONE) return 1;
-  if (current_character == CAT_CHARACTER && CAT_CHARACTER == nextPos_character) {
+  if (nextPos_character == NO_CHARACTER) return 1;
+  if (current_character == CAT_CHARACTER && current_character == nextPos_character) 
     return 1;
-  }
   return 0;
+}
+
+void dogRandomMove() {
+  int direction_number;
+  enum MOVEMENT movement;
+  int new_x;
+  int new_y;
+  // UP = 1; DOWN = 2;RIGHT = 3;LEFT = 4
+  for (int dog_index = 0; dog_index < DOG_COUNT; dog_index++) {
+    int x = dogs[dog_index].x;
+    int y = dogs[dog_index].y;
+
+    for (int i = 0; i < 5; i++) // max attempts: 3 times
+    {
+      new_x = x;
+      new_y = y;
+      direction_number = (rand() % (4)) + 1;
+      switch (direction_number)
+      {
+      case 1:
+        //UP
+        movement = UP;
+        new_y = y - 1;
+        break;
+      case 2:
+        //RIGHT
+        movement = RIGHT;
+        new_x = x + 1;
+        break;
+      case 3:
+        //DOWN
+        movement = DOWN;
+        new_y = y + 1;
+        break;
+      case 4:
+        //LEFT
+        movement = LEFT;
+        new_x = x - 1;
+        break;
+      }
+      if (canMove(x, y, movement)) break;
+    }
+
+    printf("FINISHED!\n");
+    addFlag(&map[new_y][new_x], FLAG_DOG);
+    removeFlag(&map[y][x], FLAG_DOG);
+
+    dogs[dog_index].x = new_x;
+    dogs[dog_index].y = new_y;
+  }
+}
+
+int freeMovementCount(int x,int y,enum MOVEMENT movement[]) {
+  int counter = 0;
+  if (canMove(x, y, UP)) movement[counter++] = UP;
+  if (canMove(x, y, RIGHT)) movement[counter++] = RIGHT;
+  if (canMove(x, y, DOWN)) movement[counter++] = DOWN;
+  if (canMove(x, y, LEFT)) movement[counter++] = RIGHT;
+  return counter;
 }
